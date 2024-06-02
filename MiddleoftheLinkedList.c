1c68db2f9c35a263540fd37b34f2b7feb26c6c2c
hadil belhadj hassine <hadilbelhajhassine673@gmail.com>
2024-06-02 16:33:47 +0100

    middlelist

--- MiddleoftheLinkedList.c
+++ MiddleoftheLinkedList.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    printf("Hello, Feature Branch!\n");
-    return ;
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+
+
+
+struct ListNode* middleNode(struct ListNode* head) {
+     if (head == NULL) {
+        return NULL;
+    }
+
+    struct ListNode* slow = head;
+    struct ListNode* fast = head;
+
+    while (fast != NULL && fast->next != NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    return slow;
+
 }
+
+ 
